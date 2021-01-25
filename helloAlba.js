298a191d51214f792ae70c7373bc4a533009e6c9
NotAlba <alab2000.2014@gmail.com>
2021-01-25 18:51:08 +0100

    Primer commit

---
+++ helloAlba.js
@@ -0,0 +1,19 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', {nombre: 'Anonimo'});
+});
+
+app.post('/resultado', function(req, res) {
+    var nombre = req.body.nombre;
+    var genero = req.body.gender;
+    //res.send('<h1>Hello</h1> '+nombre);
+    res.render('form', {nom: nombre, genere:genero});
+});
+
+app.listen(4000, () => console.log('Example app listening on port 4000!'));
